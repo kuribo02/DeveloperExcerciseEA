9a50bdfdd6c41d91ef479d38a4c6c97648bebecd
Matias <matias_db02@hotmail.com>
2016-04-05 21:18:54 -0300

    Added files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function openSideMenu2() {
+    document.getElementById("sideMenu").style.display = "block";
+}
+
+function closeSideMenu2() {
+    document.getElementById("sideMenu").style.display = "none";
+}
+
+function openSideMenu() {
+    document.getElementById("sideMenu").style.left = "0";
+}
+
+function closeSideMenu() {
+    document.getElementById("sideMenu").style.left = "-350px";
+}
